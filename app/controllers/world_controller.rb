3228895d9c0db3cbbc34254c56fca0d5559b5564
Tomoyuki Ito <tomoyuki.atago@gmail.com>
2014-07-21 12:02:39 +0900

    worldコントローラ追加

---
+++ app/controllers/world_controller.rb
@@ -0,0 +1,4 @@
+class WorldController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   # get 'welcome/index'
-  resources :welcome, :only => :index
   root 'welcome#index'
+  resources :welcome, :only => :index
+  resources :world, :only => :index
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
