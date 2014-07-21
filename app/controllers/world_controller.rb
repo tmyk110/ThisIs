7975c20969425edc877d8ff52610c7a7acf16691
Tomoyuki Ito <tomoyuki.atago@gmail.com>
2014-07-21 20:57:37 +0900

    worldにregionとcountryを表示

--- app/controllers/world_controller.rb
+++ app/controllers/world_controller.rb
@@ -1,4 +1,14 @@
 class WorldController < ApplicationController
   def index
+    @groups = Array.new
+    for region in Region.all do
+      group = Array.new
+      group.push(region.name)
+      countryNames = Country.where(:region_id => region.id).pluck(:name)
+      for name in countryNames do
+        group.push(name)
+      end
+      @groups.push(group)
+    end
   end
 end
